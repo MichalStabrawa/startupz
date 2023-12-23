d10463d553328125c2fc123f3aa64c23d9dd1cdb
Michal Stabrawa <mistaaqua@gmail.com>
2023-12-23 20:25:39 +0100

    add clearTimeout

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import logo from "./logo.svg";
 import "./App.css";
 import HeaderComponent from "./components/Header/HeaderComponent";
 import SectionFirstComponent from "./components/SectionContent/SectionFirst/SectionFirstComponent";
+import SectionSecondComponent from "./components/SectionContent/SectionSecond/SectionSecondComponent";
 function App() {
   return (
     <div className="App">
       <main className="main_wrapper"></main>
       <HeaderComponent />
       <SectionFirstComponent/>
+      <SectionSecondComponent/>
     </div>
   );
 }
---
+++ src/components/SectionContent/SectionSecond/SectionSecondComponent.js
@@ -0,0 +1,73 @@
+import { useState, useEffect } from "react";
+import classes from "./SectionSecondComponent.module.scss";
+import SectionComponent from "../../UI/Section/SectionComponent";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+
+import imgLeft from "../../../assets/Thumb.svg";
+import imgRight from "../../../assets/Thumb2.svg";
+import xImg from "../../../assets/x.svg";
+import iconImg from "../../../assets/gr30.svg";
+
+const SectionSecondComponent = () => {
+  const [flag, setFlag] = useState(false);
+
+  const handleModal = () => {
+    setFlag(false);
+  };
+
+  useEffect(() => {
+    setTimeout(() => {
+      setFlag(true);
+    }, 800);
+
+    return () => {
+      clearTimeout();
+    };
+  }, []);
+  return (
+    <SectionComponent>
+      <div className={classes.section_second}>
+        <Container>
+          <Row>
+            <Col>
+              <div className={classes.section_second_wrapper}>
+                <figure>
+                  <img src={imgLeft} />
+                </figure>
+
+                <h2 className={classes.section_second_title}>
+                  We love solving problems!
+                </h2>
+                <figure>
+                  <img src={imgRight} />
+                </figure>
+              </div>
+              {flag && (
+                <div className={classes.modal}>
+                  <div className={classes.modal_content}>
+                    <div className={classes.rectangle}>
+                      Hi! I am Ben, your virtual assistant. How can I make your
+                      day better?
+                    </div>
+                    <button onClick={handleModal} className={classes.close}>
+                      <img src={xImg} />
+                    </button>
+                  </div>
+                  <div className={classes.modal_icon}>
+                    <figure className={classes.modal_icon_wrapper}>
+                      <img src={iconImg}></img>
+                    </figure>
+                  </div>
+                </div>
+              )}
+            </Col>
+          </Row>
+        </Container>
+      </div>
+    </SectionComponent>
+  );
+};
+
+export default SectionSecondComponent;
